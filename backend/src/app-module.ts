5551b977bab17b37db0993491a9a841334f7804b
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-05-28 12:03:06 -0300

    feat: create main module class

---
+++ backend/src/app-module.ts
@@ -0,0 +1,11 @@
+import type { GenericClass } from '@core/shared/types/generic-class.type'
+
+export class AppModule {
+	getAllControllers(): Array<GenericClass> {
+		return []
+	}
+
+	getAllEvents(): Array<GenericClass> {
+		return []
+	}
+}
---
+++ backend/src/core/shared/types/generic-class.type.ts
@@ -0,0 +1,1 @@
+export type GenericClass<T = unknown> = new () => T
